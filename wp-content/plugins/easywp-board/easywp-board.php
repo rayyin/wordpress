6fe5769d492b832618c730f5c409b352cbc4eb18
Ray Yin <ryin005@gmail.com>
2014-11-03 17:56:52 +0800

    [easywp-board] Added a temp init script

---
+++ wp-content/plugins/easywp-board/easywp-board.php
@@ -0,0 +1,23 @@
+<?php
+/*
+	Plugin Name: EasyWp Board
+	Plugin URI: http://raywp.com:8064/extend/plugins/wp-bulletin-board/
+	Description: An easy to use Activity board for Wordpress
+	Version: 1.0.0
+	Author: Ray Yin
+	Author URI: http://rayyin.com
+	License: GPLv2 or later
+*/
+
+/**
+ * @brief Declare constants for generic use and for checking to avoid a direct call from the Web
+ **/
+define('__WP_BOARD__',   TRUE);
+
+
+
+
+
+
+
+
